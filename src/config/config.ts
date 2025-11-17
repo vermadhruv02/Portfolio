97ebb30511b21e9d8e255f605c6584a8b847ca7e
vermadhruv02 <dhruvverma.dev@gmail.com>
2025-11-18 03:15:09 +0530

    chore: Comment out console.log statements in getProject function

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,9 @@
 const URL = `${import.meta.env.VITE_BACKEND_URL}/api/projects` ;
 async function getProject() {
-  console.log(URL);
+  // console.log(URL);
   const data = await fetch(URL);
   const result = await data.json();
-  console.log(`result: ${JSON.stringify(result)}`);
+  // console.log(`result: ${JSON.stringify(result)}`);
   return result;
 }
 export default getProject;
